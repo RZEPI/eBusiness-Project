0cd5dd4aaa8bcc82e90bd6b42f77fefd64a0788a
Paweł Rzepecki <pawel@desktop-pusi448-2.home>
2023-12-06 18:49:54 +0100

    typo corrected, added saving categories assigned to id to the file

--- scripts/api-script.py
+++ scripts/api-script.py
@@ -1,4 +1,5 @@
 import json
+import sys
 import io
 from random import randint
 from prestapyt import PrestaShopWebServiceDict
@@ -13,12 +14,17 @@ API_KEY = "2ACW94YGW4W8H53JCZYGUQG1Y4VYJWM8"
 IS_SECURE = False
 URI = "http://localhost:8080"
 URI_SECURE = "https://localhost:8080"
+HOME_CAT_ID = 2
+
 
 def load_json_data(file_name=CATEGORIES_FILE):
     with open(f"{DATA_PATH}{file_name}", 'r') as file:
         json_data = json.loads(file.read())
     return json_data
 
+def save_json_data(data, file_name="categories_ids.json"):
+    with open(f"{DATA_PATH}{file_name}", 'w') as file:
+        file.write(json.dumps(data))
 
 def make_url(api_path=""):
     if IS_SECURE:
@@ -77,14 +83,13 @@ def add_images(product_id, images):
 
         prestashop.add(f"images/products/{product_id}", files=[('image', image, image_content)])
 
-def add_products(products, categories, subcategories):
+def add_products(products, subcategories):
     product_dict = prestashop.get('products', options={'schema': 'blank'})
 
     del product_dict["product"]["position_in_category"]   
     del product_dict["product"]["associations"]["combinations"]
 
     for product in products:
-        category_id = categories.get(product["category"], 1)
         subcategory_id = match_subcategory(product, subcategories)
         price = round(float(product["price"]) / 1.23, 2)
 
@@ -99,29 +104,35 @@ def add_products(products, categories, subcategories):
         product_dict["product"]["id_shop_default"] = 1
         product_dict["product"]["reference"] = product["id"]
         product_dict["product"]["state"] = 1
-        product_dict["product"]["avaliable_for_order"] = 1
+        product_dict["product"]["available_for_order"] = 1
         product_dict["product"]["show_price"] = 1
         product_dict["product"]["meta_title"]["language"]["value"] = product["name"]
         product_dict["product"]["meta_description"]["language"]["value"] = product["description"].split(".")[0]
         product_dict["product"]["associations"]["categories"] = {
             "category": [
-                {"id": 2},
-                {"id": category_id },
-                {"id": subcategory_id}
+                {"id": HOME_CAT_ID},
+                {"id": subcategory_id }
             ]
         }
         product_id = prestashop.add('products', product_dict)['prestashop']['product']['id']
         set_quantity(product_id)
         add_images(product_id, product["images"])
 
+if len(sys.argv) > 1:
+    key = str(sys.argv[1]).strip()
+else:
+    key = API_KEY
 
+print(key)
 url = make_url()
-prestashop = PrestaShopWebServiceDict(url, API_KEY)
+prestashop = PrestaShopWebServiceDict(url, key)
 
 categories = load_json_data()
 subcategories = load_json_data(SUBCATEGORIES_FILE)
-categories_assigned = add_categories(categories)
-subcategories_assigned = add_categories(subcategories, categories_assigned)
 
+categories_assigned = add_categories(categories)
+save_json_data(categories_assigned)
+
+subcategories_assigned = add_categories(subcategories, categories_assigned)
 products = load_json_data(PRODUCTS_FILE)
-add_products(products, categories_assigned,subcategories_assigned)
+add_products(products,subcategories_assigned)
